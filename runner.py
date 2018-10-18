226ccb4cb73a3f99a30d569a4bd6762f5852cb38
himanshu3006 <32892920+himanshu3006@users.noreply.github.com>
2018-10-18 10:33:24 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -134,16 +134,16 @@ if __name__ == '__main__':
     try:
 
         if command_args.build_id:
-            print "running"
+            print ("running")
             run_robot('True',command_args.build_id)
         else:
             run_robot()
 
     finally:
-        print "finally"
+        print ("finally")
         complete_dir_path = os.path.join(os.getcwd(), "logs" + os.sep + command_args.build_id)
         complete_xml_path = os.path.join(complete_dir_path, "output.xml")
-        print complete_xml_path
+        print (complete_xml_path)
         result = ExecutionResult(complete_xml_path)
         #result.suite.visit(PrintTestInfo(command_args.build_id))
 
